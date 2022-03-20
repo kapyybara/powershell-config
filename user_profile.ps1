d43b8c04710ada848fdcf9cf6f868d799e53046a
Phạm Nhật Tiến <83960491+1wku@users.noreply.github.com>
2022-03-20 14:57:41 +0700

    add alias

--- user_profile.ps1
+++ user_profile.ps1
@@ -31,20 +31,39 @@ Set-Alias y yarn
 Set-Alias c cat
 Set-Alias n nvim 
 Set-Alias p pwd 
+Set-Alias open explorer
 
 # Common App
 Set-Alias pm 'C:\Users\beo16\AppData\Local\Postman\Postman.exe'
 Set-Alias fm 'C:\Users\beo16\AppData\Local\Figma\Figma.exe'
+Set-Alias chr 'C:\Program Files\Google\Chrome\Application\chrome.exe'
+
+
 
 ## Git
 function Set-GitCommit { & git commit -m $args }
-Set-Alias commit  Set-GitCommit
+Set-Alias commit  Set-GitCommit -Force -Option AllScope
 
 function Set-GitAdd { & git add $args }
-Set-Alias add  Set-GitAdd
+Set-Alias add  Set-GitAdd -Force -Option AllScope
 
 function Set-GitPush { & git push $args }
-Set-Alias push Set-GitPush
+Set-Alias push Set-GitPush -Force -Option AllScope
+
+function Set-GitFetch { & git fetch $args }
+Set-Alias fetch Set-GitFetch -Force -Option AllScope
+
+function Set-GitCheckout { & git checkout $args }
+Set-Alias check Set-GitFetch -Force -Option AllScope
+
+function Set-GitBranch { & git branch $args }
+Set-Alias br Set-GitBranch -Force -Option AllScope
+
+function Get-GitRemote { & git remote -v $args }
+Set-Alias remote Get-GitRemote -Force -Option AllScope
+
+function Get-GitPull { & git pull $args }
+Set-Alias pull Get-GitPull -Force -Option AllScope
 
 # Ultilities
 function which ($command) {
