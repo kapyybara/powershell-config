0ae687caf6adb6af1de33579bd6a85e067442313
1wku <beo1692@gmail.com>
2022-04-15 19:28:15 +0700

    to update window

--- user_profile.ps1
+++ user_profile.ps1
@@ -33,6 +33,7 @@ Set-Alias n nvim
 Set-Alias p pwd 
 Set-Alias j z 
 Set-Alias open explorer
+Set-Alias fuck thefuck 
 
 # Tree
 function Set-Tree { & treee -f --ignore 'node_modules/, .git/, .gitignore' -a $args }
@@ -43,6 +44,9 @@ Set-Alias pm 'C:\Users\beo16\AppData\Local\Postman\Postman.exe'
 Set-Alias fm 'C:\Users\beo16\AppData\Local\Figma\Figma.exe'
 Set-Alias chr 'C:\Program Files\Google\Chrome\Application\chrome.exe'
 Set-Alias mgdb 'C:\Users\beo16\AppData\Local\MongoDBCompass\MongoDBCompass.exe'
+Set-Alias zl 'C:\Users\beo16\AppData\Local\Programs\Zalo\Zalo.exe' 
+Set-Alias sotree 'C:\Users\beo16\AppData\Local\SourceTree\SourceTree.exe' 
+
 
 # XXX Web access
 # Google search
@@ -57,41 +61,36 @@ function Set-YoutubeSearch {
 	start chrome https://www.youtube.com/results?search_query=$search_query
 }
 Set-Alias ytb Set-YoutubeSearch 
-# Zalo
-function Set-Zalo {
-	start chrome chat.zalo.me
-}
-Set-Alias zl Set-Zalo 
 
 # XXX Web access
 
 # XXX Git
-function Set-GitCommit { & git commit -m $args }
-Set-Alias commit  Set-GitCommit -Force -Option AllScope
-
 function Set-GitAdd { & git add $args }
-Set-Alias add  Set-GitAdd -Force -Option AllScope
+Set-Alias gad  Set-GitAdd -Force -Option AllScope
+
+function Set-GitCommit { & git commit -m $args }
+Set-Alias gcm  Set-GitCommit -Force -Option AllScope
 
 function Set-GitPush { & git push $args }
-Set-Alias push Set-GitPush -Force -Option AllScope
+Set-Alias gp Set-GitPush -Force -Option AllScope
 
 function Set-GitPushOrigin { & git push origin $args }
-Set-Alias po Set-GitPushOrigin -Force -Option AllScope
+Set-Alias gpo Set-GitPushOrigin -Force -Option AllScope
 
 function Set-GitFetch { & git fetch $args }
-Set-Alias fetch Set-GitFetch -Force -Option AllScope
+Set-Alias gf Set-GitFetch -Force -Option AllScope
 
 function Set-GitCheckout { & git checkout $args }
-Set-Alias check Set-GitFetch -Force -Option AllScope
+Set-Alias gco Set-GitFetch -Force -Option AllScope
 
 function Set-GitBranch { & git branch $args }
-Set-Alias br Set-GitBranch -Force -Option AllScope
+Set-Alias gbr Set-GitBranch -Force -Option AllScope
 
 function Get-GitRemote { & git remote -v $args }
-Set-Alias remote Get-GitRemote -Force -Option AllScope
+Set-Alias grmt Get-GitRemote -Force -Option AllScope
 
 function Get-GitPull { & git pull $args }
-Set-Alias pull Get-GitPull -Force -Option AllScope
+Set-Alias gpl Get-GitPull -Force -Option AllScope
 # XXX Git
 
 # Ultilities
